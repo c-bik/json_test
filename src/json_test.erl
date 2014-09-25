fc556d8b956fc4d40d2f3c3a26777695d68bc04f
Bikram Chatterjee <razorpeak@gmail.com>
2014-09-25 10:47:21 +0200

    not compiling

---
+++ src/json_test.erl
@@ -0,0 +1,16 @@
+-module(json_test).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    json_test_sup:start_link().
+
+stop(_State) ->
+    ok.
